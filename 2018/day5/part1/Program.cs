2679d50d99ed5b057ae7e031b6735bb9e81a5484
Martijn Kooij <post@martijnkooij.nl>
2018-12-05 08:56:49 +0100

    Day 5 - Part 1

---
+++ 2018/day5/part1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            var unitTypes = new List<string>();
+            for (int i = 0; i < 26; i++)
+            {
+                unitTypes.Add(((char) (65 + i)).ToString() + ((char) (65 + 32 + i)).ToString());
+                unitTypes.Add(((char) (65 + 32 + i)).ToString() + ((char) (65 + i)).ToString());
+            }
+
+            Console.WriteLine($"Starting with input length {input.Length}");
+
+            while (true)
+            {
+                var inputBeforeReacting = input;
+                foreach (var unitType in unitTypes)
+                {
+                    input = input.Replace(unitType, "");
+                }
+
+                if (inputBeforeReacting == input) {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"The answer is {input} with length {input.Length}");
+        }
+    }
+}
