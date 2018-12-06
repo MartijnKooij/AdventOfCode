ee22babc095569bbfd176b133b09d544ef01b6d4
Martijn Kooij <post@martijnkooij.nl>
2018-12-06 19:21:56 +0100

    Day 6 - part 1

--- 2018/day6/part1/Program.cs
+++ 2018/day6/part1/Program.cs
@@ -7,29 +7,77 @@ namespace part1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var input = File.ReadAllLines("input.txt");
-            List<Point> points = new List<Point>();
-            for (int i = 0; i < input.Length; i++)
+            var points = new List<Point>();
+            for (var i = 0; i < input.Length; i++)
             {
-                string values = input[i];
-                points.Add(new Point(i, values));
+                var values = input[i];
+                points.Add(new Point(i + 1, values));
             }
 
-            string[,] area = DetermineArea(points);
+            var area = DetermineArea(points);
 
-            for (int x = 0; x < area.GetLength(0); x++)
+            for (var x = 0; x < area.GetLength(0); x++)
             {
-                for (int y = 0; y < area.GetLength(1); y++)
+                for (var y = 0; y < area.GetLength(1); y++)
                 {
                     var source = new Point(0, $"{x},{y}");
+                    var shortestDistance = int.MaxValue;
+                    var shortestPointId = -1;
                     foreach (var destination in points)
                     {
-                        int distance = Distance(source, destination);
+                        if (source.X == destination.X && source.Y == destination.Y)
+                        {
+                            shortestPointId = destination.Id;
+                            break;
+                        }
+
+                        var distance = Distance(source, destination);
+                        if (distance < shortestDistance)
+                        {
+                            shortestDistance = distance;
+                            shortestPointId = destination.Id;
+                        }
+                        else if (distance == shortestDistance)
+                        {
+                            shortestPointId = 0;
+                        }
+
                     }
+
+                    area[x, y] = shortestPointId;
                 }
             }
+
+            var pointIdAreas = new List<Point>();
+            for (var x = 0; x < area.GetLength(0); x++)
+            {
+                for (var y = 0; y < area.GetLength(1); y++)
+                {
+                    pointIdAreas.Add(new Point(area[x, y], $"{x},{y}"));
+                }
+            }
+
+            for (var x = 0; x < area.GetLength(0); x++)
+            {
+                pointIdAreas.RemoveAll(point => point.Id == area[x, 0]);
+                pointIdAreas.RemoveAll(point => point.Id == area[x, area.GetLength(1) - 1]);
+            }
+            for (var y = 0; y < area.GetLength(1); y++)
+            {
+                pointIdAreas.RemoveAll(point => point.Id == area[0, y]);
+                pointIdAreas.RemoveAll(point => point.Id == area[area.GetLength(0) - 1, y]);
+            }
+
+            var safestAreaSize = pointIdAreas
+                                    .GroupBy(x => x.Id)
+                                    .Select(x => x.Count())
+                                    .OrderByDescending(x => x)
+                                    .First();
+
+            Console.WriteLine($"The answer is {safestAreaSize}");
         }
 
         private static int Distance(Point source, Point destination)
@@ -37,10 +85,10 @@ namespace part1
             return Math.Abs(destination.X - source.X) + Math.Abs(destination.Y - source.Y);
         }
 
-        private static string[,] DetermineArea(List<Point> points)
+        private static int[,] DetermineArea(List<Point> points)
         {
-            int highX = 0;
-            int highY = 0;
+            var highX = 0;
+            var highY = 0;
 
             foreach (var point in points)
             {
@@ -54,18 +102,19 @@ namespace part1
                 }
             }
 
-            return new string[highX, highY];
+            return new int[highX, highY];
         }
     }
 
-    class Point {
-        public int Id {get;}
-        public int X {get;}
-        public int Y {get;}
-        
+    class Point
+    {
+        public int Id { get; }
+        public int X { get; }
+        public int Y { get; }
+
         public Point(int id, string values)
         {
-            Id = Id;
+            Id = id;
             var inputValues = values.Split(',');
             X = Int32.Parse(inputValues[0]);
             Y = Int32.Parse(inputValues[1]);
