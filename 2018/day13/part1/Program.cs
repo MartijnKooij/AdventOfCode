d708ef4418a5efa2029ba7340062e4754f0a2617
Martijn Kooij <post@martijnkooij.nl>
2018-12-13 09:15:19 +0100

    Day 13 - Getting started

---
+++ 2018/day13/part1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("testinput.txt");
+            foreach (var line in lines)
+            {
+                foreach (var trackCharacter in line)
+                {
+                    
+                }
+            }
+        }
+    }
+}
