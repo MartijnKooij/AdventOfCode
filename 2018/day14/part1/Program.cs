2b9efad5200987687a2a5ff44df220212ccb5ac2
Martijn Kooij <post@martijnkooij.nl>
2018-12-14 20:09:50 +0100

    Day 14

--- 2018/day14/part1/Program.cs
+++ 2018/day14/part1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace part1
 {
@@ -6,7 +8,42 @@ namespace part1
     {
         static void Main(string[] args)
         {
-            var input = "37"; //825401
+            var recipes = new List<int> { 3, 7 };
+            var totalRecipes = 825401;
+            var elf1Index = 0;
+            var elf2Index = 1;
+
+            while (true)
+            {
+                var elf1Recipe = recipes[elf1Index];
+                var elf2Recipe = recipes[elf2Index];
+
+                var sum = elf1Recipe + elf2Recipe;
+                var digits = GetDigits(sum);
+                recipes.AddRange(digits);
+
+                elf1Index = (elf1Index + recipes[elf1Index] + 1) % recipes.Count;;
+                elf2Index = (elf2Index + recipes[elf2Index] + 1) % recipes.Count;;
+
+                //Console.WriteLine(string.Join(" ", recipes));
+
+                if (recipes.Count >= totalRecipes + 10) {
+                    Console.WriteLine($"The answer is {string.Join("", recipes.Skip(totalRecipes).Take(10))}");
+                    break;
+                }
+            }
+        }
+
+        public static List<int> GetDigits(int value)
+        {
+            var values = new List<int>();
+            if (value >= 10)
+            {
+                values.AddRange(GetDigits(value / 10));
+            }
+            values.Add(value % 10);
+
+            return values;
         }
     }
 }
--- 2018/day14/part2/Program.cs
+++ 2018/day14/part2/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace part2
+namespace part1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var recipes = new List<int> { 3, 7 };
+            var totalRecipes = "37";
+            var elf1Index = 0;
+            var elf2Index = 1;
+
+            while (true)
+            {
+                var elf1Recipe = recipes[elf1Index];
+                var elf2Recipe = recipes[elf2Index];
+
+                var sum = elf1Recipe + elf2Recipe;
+                var digits = GetDigits(sum);
+
+                totalRecipes += sum.ToString();
+                recipes.AddRange(digits);
+
+                elf1Index = (elf1Index + recipes[elf1Index] + 1) % recipes.Count;
+                elf2Index = (elf2Index + recipes[elf2Index] + 1) % recipes.Count;
+
+                //Console.WriteLine(string.Join(" ", recipes));
+
+                if (totalRecipes.Contains("825401")) {
+                    Console.WriteLine($"The answer is {totalRecipes} {totalRecipes.IndexOf("825401")}");
+                    break;
+                }
+            }
+        }
+
+        public static List<int> GetDigits(int value)
+        {
+            var values = new List<int>();
+            if (value >= 10)
+            {
+                values.AddRange(GetDigits(value / 10));
+            }
+            values.Add(value % 10);
+
+            return values;
         }
     }
 }
