a2b20397f88ea1fba8deef9ec04598815e9c7200
Martijn Kooij <post@martijnkooij.nl>
2018-12-16 17:15:14 +0100

    Day 16 WiP

--- 2018/day16/part1/UnitTest1.cs
+++ 2018/day16/part1/UnitTest1.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using Xunit;
 
 namespace part1
@@ -19,7 +21,43 @@ namespace part1
             var testData = challenge.ParseInput(new[] {
                 "Before: [0, 3, 3, 0]", "5 0 2 1", "After:  [0, 0, 3, 0]"});
 
-            Assert.Equal(3, testData.Count);
+            Assert.Single(testData);
+            Assert.Equal(new int[] { 0, 3, 3, 0 }, testData[0].Inputs);
+            Assert.Equal(new int[] { 5, 0, 2, 1 }, testData[0].Instructions);
+            Assert.Equal(new int[] { 0, 0, 3, 0 }, testData[0].Outputs);
+        }
+
+        [Fact]
+        public void ParsesMultipleInputs()
+        {
+            var testData = challenge.ParseInput(new[] {
+                "Before: [0, 3, 3, 0]", "5 0 2 1", "After:  [0, 0, 3, 0]",
+                "",
+                "Before: [2, 2, 1, 0]", "3 0 2 3", "After:  [2, 2, 1, 4]"
+                });
+
+            Assert.Equal(2, testData.Count);
+            Assert.Equal(new int[] { 0, 3, 3, 0 }, testData[0].Inputs);
+            Assert.Equal(new int[] { 5, 0, 2, 1 }, testData[0].Instructions);
+            Assert.Equal(new int[] { 0, 0, 3, 0 }, testData[0].Outputs);
+            Assert.Equal(new int[] { 2, 2, 1, 0 }, testData[1].Inputs);
+            Assert.Equal(new int[] { 3, 0, 2, 3 }, testData[1].Instructions);
+            Assert.Equal(new int[] { 2, 2, 1, 4 }, testData[1].Outputs);
+        }
+
+        [Fact]
+        public void ParseShouldStopAfterDoubleNewLine()
+        {
+            var testData = challenge.ParseInput(new[] {
+                "Before: [0, 3, 3, 0]", "5 0 2 1", "After:  [0, 0, 3, 0]",
+                "",
+                "Before: [2, 2, 1, 0]", "3 0 2 3", "After:  [2, 2, 1, 4]",
+                "",
+                "",
+                "Before: [2, 2, 1, 0]", "3 0 2 3", "After:  [2, 2, 1, 4]"
+                });
+
+            Assert.Equal(2, testData.Count);
         }
     }
 
@@ -27,7 +65,53 @@ namespace part1
     {
         public List<TestData> ParseInput(string[] lines)
         {
-            var testData = new List<TestData>();
+            var testData = new List<TestData>{
+                new TestData()
+            };
+            var lineType = -1;
+            var testDataIndex = 0;
+
+            foreach (var line in lines.Select(x => x.Replace("  ", " ").Trim()))
+            {
+                lineType++;
+
+                if (lineType == 0 && string.IsNullOrEmpty(line))
+                {
+                    testData.RemoveAt(testDataIndex - 1);
+                    break;
+                }
+
+                switch (lineType)
+                {
+                    case 0:
+                        testData[testDataIndex].Inputs = line
+                            .Replace("Before: [", "")
+                            .Replace("]", "")
+                            .Split(",")
+                            .Select(x => int.Parse(x.Trim()))
+                            .ToArray();
+                        break;
+                    case 1:
+                        testData[testDataIndex].Instructions = line
+                            .Split(" ")
+                            .Select(x => int.Parse(x.Trim()))
+                            .ToArray();
+                        break;
+                    case 2:
+                        testData[testDataIndex].Outputs = line
+                            .Replace("After: [", "")
+                            .Replace("]", "")
+                            .Split(",")
+                            .Select(x => int.Parse(x.Trim()))
+                            .ToArray();
+                        break;
+                    case 3:
+                        testData.Add(new TestData());
+                        testDataIndex++;
+                        lineType = -1;
+                        break;
+                }
+            }
 
             return testData;
         }
@@ -37,7 +121,7 @@ namespace part1
     {
         public int[] Inputs { get; set; }
 
-        public int[] Instruction { get; set; }
+        public int[] Instructions { get; set; }
 
         public int[] Outputs { get; set; }
 
