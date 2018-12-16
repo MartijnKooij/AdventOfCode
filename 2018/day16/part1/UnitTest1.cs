0233660820752f52c0464e9ab5ac1d5fae8131c7
Martijn Kooij <post@martijnkooij.nl>
2018-12-16 12:01:27 +0100

    Day 16 - WiP

--- 2018/day16/part1/UnitTest1.cs
+++ 2018/day16/part1/UnitTest1.cs
@@ -1,14 +1,45 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace part1
 {
-    public class UnitTest1
+    public class Day16Tests
     {
-        [Fact]
-        public void Test1()
+        private Day16 challenge;
+
+        public Day16Tests()
         {
-            Assert.Equal(1, 1);
+            challenge = new Day16();
         }
+
+        [Fact]
+        public void ParsesSingleInput()
+        {
+            var testData = challenge.ParseInput(new[] {
+                "Before: [0, 3, 3, 0]", "5 0 2 1", "After:  [0, 0, 3, 0]"});
+
+            Assert.Equal(3, testData.Count);
+        }
+    }
+
+    public class Day16
+    {
+        public List<TestData> ParseInput(string[] lines)
+        {
+            var testData = new List<TestData>();
+
+            return testData;
+        }
+    }
+
+    public class TestData
+    {
+        public int[] Inputs { get; set; }
+
+        public int[] Instruction { get; set; }
+
+        public int[] Outputs { get; set; }
+
     }
 }
