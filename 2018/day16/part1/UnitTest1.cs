9b8773e290ffb9f4eeaf9845e78560bed16f252f
Martijn Kooij <post@martijnkooij.nl>
2018-12-16 10:47:26 +0100

    Day 16 - WiP

---
+++ 2018/day16/part1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace part1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
