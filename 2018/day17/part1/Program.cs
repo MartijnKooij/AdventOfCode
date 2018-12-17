b55017159858d6415a88bca8a826cd9d30eb1580
Martijn Kooij <post@martijnkooij.nl>
2018-12-17 09:08:31 +0100

    Day 17 WiP

---
+++ 2018/day17/part1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            var map = new char[2000, 2000];
+            foreach (var line in input)
+            {
+                var values = line.Split(',');
+                int[] xValues;
+                int[] yValues;
+                if (values[0].Trim().StartsWith("x"))
+                {
+                    xValues = ParseValue(values[0]);
+                    yValues = ParseValue(values[1]);
+                }
+                else
+                {
+                    yValues = ParseValue(values[0]);
+                    xValues = ParseValue(values[1]);
+                }
+
+                foreach (var x in xValues)
+                {
+                    foreach (var y in yValues)
+                    {
+                        map[x, y] = '#';
+                    }
+                }
+            }
+
+            LogMap(map);
+        }
+
+        private static void LogMap(char[,] map)
+        {
+            File.WriteAllText("output.txt", "");
+            using (var file = new StreamWriter("output.txt", true, Encoding.ASCII))
+            {
+                file.WriteLine("");
+                for (var y = 0; y < map.GetLength(1); y++)
+                {
+                    var line = "";
+                    for (var x = 0; x < map.GetLength(0); x++)
+                    {
+                        line += map[x, y];
+                    }
+                    file.WriteLine(line);
+                    file.WriteLine("");
+                }
+            }
+        }
+
+        private static int[] ParseValue(string value)
+        {
+            value = value.Trim().Replace("x=", "").Replace("y=", "").Replace("..", ".");
+            var values = value.Split('.').Select(x => int.Parse(x));
+
+            return values.ToArray();
+        }
+    }
+}
