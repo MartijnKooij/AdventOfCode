7fa25df1d68eb20204e450d3ad1638ebd6df09c6
Martijn Kooij <post@martijnkooij.nl>
2018-12-22 16:35:20 +0100

    Day 22 WiP

---
+++ 2018/day22/part1/EnumExtensions.cs
@@ -0,0 +1,69 @@
+using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace part1
+{
+    /// <summary>
+    /// Enum type extension methods
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Get the description of an enum value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerationValue"></param>
+        /// <returns></returns>
+        public static string GetDescription<T>(this T enumerationValue) where T : struct
+        {
+            Type type = enumerationValue.GetType();
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException("EnumerationValue must be of Enum type", nameof(enumerationValue));
+            }
+
+            MemberInfo[] memberInfo = type.GetMember(enumerationValue.ToString());
+            if (memberInfo.Length <= 0)
+            {
+                return enumerationValue.ToString();
+            }
+
+            object[] attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attrs.Length > 0 ? ((DescriptionAttribute) attrs[0]).Description : enumerationValue.ToString();
+        }
+
+        /// <summary>
+        /// Typed enum parsing
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static T ToEnum<T>(this string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+
+        /// <summary>
+        /// Tries to find the enum value with the given description attribute
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        public static T ToEnumByDescription<T>(this string description) where T : struct
+        {
+            var enumValues = Enum.GetValues(typeof(T));
+
+            foreach (T enumValue in enumValues)
+            {
+                var enumDescription = enumValue.GetDescription();
+                if (enumDescription == description)
+                {
+                    return enumValue;
+                }
+            }
+
+            throw new ArgumentOutOfRangeException($"Could find the enum with the given description {description}");
+        }
+    }
+}
---
+++ 2018/day22/part1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace part1
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            const int depth = 3879;
+            (int x, int y) target = (8, 713);
+            var regions = new List<Region>
+            {
+                new Region {Location = (0, 0), GeoIndex = 0, GeoType = GeoType.Me},
+                new Region {Location = target, GeoIndex = 0, GeoType = GeoType.Target}
+            };
+
+            for (var x = 0; x < 10; x++)
+            {
+                for (var y = 0; y < 720; y++)
+                {
+                    if (regions.Any(r => r.Location.x == x && r.Location.y == y))
+                    {
+                        continue;
+                    }
+
+                    var geoIndex = GetGeoIndex(regions, x, y);
+                    var erosionLevel = (geoIndex + depth) % 20183;
+                    var region = new Region
+                    {
+                        Location = (x, y),
+                        GeoIndex = geoIndex,
+                        ErosionLevel = erosionLevel,
+                        GeoType = (GeoType)(erosionLevel % 3)
+                    };
+
+                    regions.Add(region);
+                }
+            }
+
+            LogMap(regions);
+        }
+
+        private static int GetGeoIndex(IReadOnlyCollection<Region> regions, int x, int y)
+        {
+            if (y == 0) return 16807 * x;
+            if (x == 0) return 48271 * y;
+
+            return GetNeighboringErosionLevel(regions, x, y);
+        }
+
+        private static int GetNeighboringErosionLevel(IReadOnlyCollection<Region> regions, int x, int y)
+        {
+            var neighbor1 = regions.Single(r => r.Location.x == x - 1 && r.Location.y == y);
+            var neighbor2 = regions.Single(r => r.Location.x == x && r.Location.y == y - 1);
+
+            return neighbor1.ErosionLevel * neighbor2.ErosionLevel;
+        }
+
+        private static void LogMap(List<Region> regions)
+        {
+            File.WriteAllText("output.txt", "");
+            var map = new string[10, 720];
+            foreach (var region in regions)
+            {
+                map[region.Location.x, region.Location.y] = region.GeoType.GetDescription();
+            }
+            using (var file = new StreamWriter("output.txt", true, Encoding.ASCII))
+            {
+                for (var y = 0; y < map.GetLength(1); y++)
+                {
+                    var line = new StringBuilder(map.GetLength(0));
+                    for (var x = 0; x < map.GetLength(0); x++)
+                    {
+                        line.Append(map[x, y]);
+                    }
+                    file.WriteLine(line);
+                }
+            }
+        }
+
+    }
+
+    class Region
+    {
+        public GeoType GeoType { get; set; }
+
+        public (int x, int y) Location { get; set; }
+
+        public int ErosionLevel { get; set; }
+
+        public int GeoIndex { get; set; }
+    }
+
+    enum GeoType
+    {
+        [Description(".")]
+        Rock = 0,
+
+        [Description("=")]
+        Wet = 1,
+
+        [Description("|")]
+        Narrow = 2,
+
+        [Description("M")]
+        Me = 3,
+
+        [Description("T")]
+        Target = 4
+    }
+}
