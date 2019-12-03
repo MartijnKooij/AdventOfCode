ad3dfeb012bb9133eafd5b5e0207ebc8f01bf670
Martijn Kooij <post@martijnkooij.nl>
2019-12-03 22:27:44 +0100

    Day1Part1

---
+++ 2019/day1/part1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input");
+            var totalFuel = 0;
+            foreach (var line in input)
+            {
+                var mass = double.Parse(line);
+                var moduleFuel = (int) Math.Floor(mass/3)  -2;
+                
+                totalFuel += moduleFuel;
+            }
+
+            Console.WriteLine(totalFuel);
+        }
+    }
+}
