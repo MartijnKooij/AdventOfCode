be0df4bffcf39eab5db117c1ab26bd56ae0f818a
Martijn Kooij <post@martijnkooij.nl>
2019-12-03 23:14:05 +0100

    Day2Part1

---
+++ 2019/day2/part1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input").Split(',');
+            int[] intCodes = Array.ConvertAll(input, int.Parse);
+
+            intCodes[1] = 12;
+            intCodes[2] = 2;
+
+            var pos = 0;
+            while (true)
+            {
+                if (!ProcessOpCode(intCodes, pos))
+                {
+                    break;
+                }
+                pos += 4;
+            }
+
+            Console.WriteLine(string.Join(",", intCodes));
+        }
+
+        private static bool ProcessOpCode(int[] intCodes, int pos)
+        {
+            var opCode = intCodes[pos];
+            var value = 0;
+
+            switch (opCode)
+            {
+                case 1:
+                    value = intCodes[intCodes[pos + 1]] + intCodes[intCodes[pos + 2]];
+                    intCodes[intCodes[pos + 3]] = value;
+
+                    return true;
+                case 2:
+                    value = intCodes[intCodes[pos + 1]] * intCodes[intCodes[pos + 2]];
+                    intCodes[intCodes[pos + 3]] = value;
+
+                    return true;
+                case 99:
+
+                    return false;
+                default:
+                    return true;
+            }
+        }
+    }
+}
---
+++ 2019/day2/part2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input");
+        }
+    }
+}
