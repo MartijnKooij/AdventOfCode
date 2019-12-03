f51a9e7742d6e03829b6cc646ffe6bbc8a951785
Martijn Kooij <post@martijnkooij.nl>
2019-12-03 22:42:15 +0100

    Day1Part2

---
+++ 2019/day1/part2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input");
+            var totalFuel = 0;
+            foreach (var line in input)
+            {
+                int moduleFuel = GetFuelNeeded(double.Parse(line));
+
+                totalFuel += moduleFuel;
+            }
+
+            Console.WriteLine(totalFuel);
+        }
+
+        private static int GetFuelNeeded(double mass)
+        {
+            var moduleFuel = (int)Math.Floor(mass / 3) - 2;
+
+            if (moduleFuel < 0)
+            {
+                moduleFuel = 0;
+            }
+
+            if (moduleFuel > 0)
+            {
+                moduleFuel += GetFuelNeeded(moduleFuel);
+            }
+
+            return moduleFuel;
+        }
+    }
+}
