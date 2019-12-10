e4f043f8595a0acedf1a9963f64ec73203888bad
Martijn Kooij <post@martijnkooij.nl>
2019-12-10 22:54:34 +0100

    Day4Part1

---
+++ 2019/day4/part1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int inputStart = 158126;
+            const int inputEnd = 624574;
+
+            var possiblePasswords = 0;
+            for (int password = inputStart; password < inputEnd; password++)
+            {
+                var passwordSpan = password.ToString().AsSpan();
+                var constainsSet = false;
+                var increaseOnly = true;
+                for (int i = 1; i < 6; i++)
+                {
+                    if (passwordSpan[i - 1] > passwordSpan[i])
+                    {
+                        increaseOnly = false;
+                        break;
+                    }
+                    if (passwordSpan[i - 1] == passwordSpan[i])
+                    {
+                        constainsSet = true;
+                    }
+                }
+
+                if (increaseOnly && constainsSet)
+                {
+                    possiblePasswords += 1;
+                }
+            }
+
+            Console.WriteLine(possiblePasswords);
+        }
+    }
+}
