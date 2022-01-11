1dab76e50001593555bdba863492094e18f6e4f5
Martijn Kooij <post@martijnkooij.nl>
2022-01-11 22:23:43 +0100

    Day 8.2 WiP

--- 2021/day-8/part-2.js
+++ 2021/day-8/part-2.js
@@ -4,10 +4,28 @@ exports.Runner = void 0;
 const read_input_1 = require("./read-input");
 class Runner {
     constructor() {
-        this.displays = (0, read_input_1.readInput)('./input.txt');
+        this.displays = (0, read_input_1.readInput)('./control.txt');
     }
     execute() {
-        console.log('input', this.displays);
+        this.displays.forEach(display => {
+            const oneSegments = display.segments.filter(s => s.length === read_input_1.defaultSegments[1].length).join();
+            const segmentLines = [
+                '',
+                '',
+                oneSegments[1],
+                '',
+                oneSegments[2],
+                ''
+            ];
+            const sevenSegments = display.segments.filter(s => s.length === read_input_1.defaultSegments[7].length).join();
+            console.log('debug', sevenSegments, segmentLines, this.linesToNumber(segmentLines));
+        });
+    }
+    linesToNumber(lines) {
+        if (lines[0] && lines[2] && lines[4]) {
+            return 7;
+        }
+        return -1;
     }
 }
 exports.Runner = Runner;
--- 2021/day-8/part-2.ts
+++ 2021/day-8/part-2.ts
@@ -5,11 +5,33 @@ export class Runner {
   private displays: Display[];
 
   constructor() {
-    this.displays = readInput('./input.txt');
+    this.displays = readInput('./control.txt');
   }
 
   execute() {
-    console.log('input', this.displays);
+    this.displays.forEach(display => {
+      const oneSegments = display.segments.filter(s => s.length === defaultSegments[1].length).join();
+      const segmentLines = [
+        '',
+        '',
+        oneSegments[1],
+        '',
+        oneSegments[2],
+        ''
+      ];
+      const sevenSegments = display.segments.filter(s => s.length === defaultSegments[7].length).join();
+      
+
+      console.log('debug', sevenSegments, segmentLines, this.linesToNumber(segmentLines));
+    });
+  }
+
+  linesToNumber(lines: string[]): number {
+    if (lines[0] && lines[2] && lines[4]) {
+      return 7;
+    }
+
+    return -1;
   }
 }
 
