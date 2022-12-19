89dd0de6e7cc1b0459d340c7731a7eaa5de37b17
Martijn Kooij <martijn.kooij@snappet.org>
2022-12-19 10:39:34 +0100

    Day 19 parsing

--- 2022/src/day19/index.ts
+++ 2022/src/day19/index.ts
@@ -1,18 +1,31 @@
 import run from 'aocrunner';
 
 type resource = 'ore' | 'clay' | 'obsidian' | 'geode';
-type bot = 'ore' | 'clay' | 'obsidian' | 'geode';
 type cost = {
   type: resource,
   cost: number
 };
-type blueprint = {
-  id: number,
-  bot: bot,
+type bot = {
+  resource: resource,
   oreCost: cost,
   clayCost: cost,
   obsidianCost: cost,
-  geodeCost: cost,
+  geodeCost: cost
+};
+class Blueprint {
+  id: number;
+  oreBot: bot;
+  clayBot: bot;
+  obsidianBot: bot;
+  geodeBot: bot;
+
+  constructor(id: number) {
+    this.id = id;
+    this.oreBot = { resource: 'ore', oreCost: { type: 'ore', cost: 0 }, clayCost: { type: 'clay', cost: 0 }, obsidianCost: { type: 'obsidian', cost: 0 }, geodeCost: { type: 'geode', cost: 0 } };
+    this.clayBot = { resource: 'clay', oreCost: { type: 'ore', cost: 0 }, clayCost: { type: 'clay', cost: 0 }, obsidianCost: { type: 'obsidian', cost: 0 }, geodeCost: { type: 'geode', cost: 0 } };
+    this.obsidianBot = { resource: 'obsidian', oreCost: { type: 'ore', cost: 0 }, clayCost: { type: 'clay', cost: 0 }, obsidianCost: { type: 'obsidian', cost: 0 }, geodeCost: { type: 'geode', cost: 0 } };
+    this.geodeBot = { resource: 'geode', oreCost: { type: 'ore', cost: 0 }, clayCost: { type: 'clay', cost: 0 }, obsidianCost: { type: 'obsidian', cost: 0 }, geodeCost: { type: 'geode', cost: 0 } };
+  }
 }
 const parseInput = (rawInput: string) => rawInput.split('\n').filter(l => !!l).map((l) => {
   const findBot = /(ore|clay|obsidian|geode)/g;
@@ -20,68 +33,66 @@ const parseInput = (rawInput: string) => rawInput.split('\n').filter(l => !!l).m
 
   const blueprintParts = l.split(':');
   const bluePrintId = parseInt(blueprintParts[0].replace('Blueprint ', ''), 10);
+  const blueprint = new Blueprint(bluePrintId);
 
   const botCostParts = blueprintParts[1].split('.');
-  console.log('parts', botCostParts.filter(b => !!b).map(b => b.trim()));
   for (const botCostPart of botCostParts.filter(b => !!b).map(b => b.trim())) {
     const botTypeMatch = botCostPart.match(findBot);
+
     if (botTypeMatch) {
-      const oreBotCost = botCostPart.replace(/Each (ore|clay|obsidian|geode) robot costs /g, '');
-      console.log('botType', botCostPart, botTypeMatch, oreBotCost);
+      const botCost = botCostPart.replace(/Each (ore|clay|obsidian|geode) robot costs /g, '');
 
-      const blueprint: blueprint = {
-        id: bluePrintId,
-        bot: botTypeMatch[0] as bot,
-        oreCost: {
-          type: 'ore',
-          cost: 0
-        },
-        clayCost: {
-          type: 'clay',
-          cost: 0
-        },
-        obsidianCost: {
-          type: 'obsidian',
-          cost: 0
-        },
-        geodeCost: {
-          type: 'geode',
-          cost: 0
-        },
-      };
+      let currentBot: bot;
+      switch (botTypeMatch[0]) {
+        case 'clay':
+          currentBot = blueprint.clayBot;
+          break;
+        case 'obsidian':
+          currentBot = blueprint.obsidianBot;
+          break;
+        case 'ore':
+          currentBot = blueprint.oreBot;
+          break;
+        case 'geode':
+          currentBot = blueprint.geodeBot;
+          break;
+        default:
+          throw new Error('unknown bot ' + botTypeMatch);
+      }
 
-      const oreBotMatches = oreBotCost.match(findCost);
-      if (oreBotMatches) {
-        for (const match of oreBotMatches.filter(m => !!m)) {
+      const costMatches = botCost.match(findCost);
+      if (costMatches) {
+        for (const match of costMatches.filter(m => !!m)) {
           const parts = match.split(' ');
           const value = parseInt(parts[0], 10);
           const type = parts[1] as resource;
           switch (type) {
             case 'clay':
-              blueprint.clayCost.cost = value;
+              currentBot.clayCost.cost = value;
               break;
             case 'obsidian':
-              blueprint.obsidianCost.cost = value;
+              currentBot.obsidianCost.cost = value;
               break;
             case 'ore':
-              blueprint.oreCost.cost = value;
+              currentBot.oreCost.cost = value;
               break;
             case 'geode':
-              blueprint.geodeCost.cost = value;
+              currentBot.geodeCost.cost = value;
               break;
             default:
               break;
           }
         }
       }
-      return blueprint;
     }
   }
+  return blueprint;
+
 });
 
 const part1 = (rawInput: string) => {
   const blueprints = parseInput(rawInput);
-  console.log(blueprints)
+  console.log(JSON.stringify(blueprints));
 
   return;
 };
