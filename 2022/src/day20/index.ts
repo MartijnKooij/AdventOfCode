f1eb43da41f36df467857666c392c6b8e5b33c2f
Martijn Kooij <post@martijnkooij.nl>
2022-12-20 23:38:52 +0100

    Day 20

--- 2022/src/day20/index.ts
+++ 2022/src/day20/index.ts
@@ -1,39 +1,50 @@
 import run from 'aocrunner';
 
-const parseInput = (rawInput: string): number[] => rawInput.trim().split('\n').filter(l => !!l).map(l => Number(l.trim()));
+type mixing = { id: number, value: number };
+const parseInput = (rawInput: string): mixing[] => rawInput
+  .split('\n')
+  .map((l, i) => ({ id: i, value: Number(l) }));
+
+const decrypt = (source: mixing[], copy: mixing[], repetitions = 1): mixing[] => {
+  for (let r = 0; r < repetitions; r++) {
+    for (const value of source) {
+      const valueIndex = copy.findIndex(m => m.id == value.id);
+      copy.splice(valueIndex, 1);
+      copy.splice((valueIndex + value.value) % copy.length, 0, value);
+    }
+  }
+
+  return copy;
+}
 
 const part1 = (rawInput: string) => {
   const source = parseInput(rawInput);
   const copy = [...source];
 
-  for (const value of source) {
-    console.log('before', value, copy.join(','));
-    const valueIndex = copy.indexOf(value);
+  decrypt(source, copy);
 
-    copy.splice(valueIndex, 1);
-    let newIndex = (valueIndex + value) % copy.length;
-    // if (newIndex === 0) newIndex = copy.length -1;
-    copy.splice(newIndex, 0, value);
-
-    console.log('after', valueIndex, newIndex, copy.join(','));
-  }
-
-  const zeroIndex = copy.indexOf(0);
-  console.log(copy.join(','), zeroIndex);
-
-  if (copy.join(',') !== '1,2,-3,4,0,3,-2') console.error('out of order...'.toUpperCase());
-
-  return [
+  const zeroIndex = copy.findIndex(m => m.value === 0);
+  const points = [
     copy[(1000 + zeroIndex) % copy.length],
     copy[(2000 + zeroIndex) % copy.length],
     copy[(3000 + zeroIndex) % copy.length]
-  ].reduce((p, c) => p + c, 0);
+  ];
+  return points.reduce((p, c) => p + c.value, 0);
 }
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const source = parseInput(rawInput).map(m => ({...m, value: m.value *= 811589153}));
+  const copy = [...source];
 
-  return;
+  decrypt(source, copy, 10);
+
+  const zeroIndex = copy.findIndex(m => m.value === 0);
+  const points = [
+    copy[(1000 + zeroIndex) % copy.length],
+    copy[(2000 + zeroIndex) % copy.length],
+    copy[(3000 + zeroIndex) % copy.length]
+  ];
+  return points.reduce((p, c) => p + c.value, 0);
 };
 
 run({
@@ -55,13 +66,20 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: '',
-      // },
+      {
+        input: `
+        1
+        2
+        -3
+        3
+        -2
+        0
+        4`,
+        expected: 1623178306,
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
