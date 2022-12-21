075b7703aa4043918c14aa4a0a7f6a0156ed627a
Martijn Kooij <martijn.kooij@snappet.org>
2022-12-21 19:31:47 +0100

    Day 15 WiP

--- 2022/src/day15/index.ts
+++ 2022/src/day15/index.ts
@@ -50,60 +50,87 @@ const part1 = (rawInput: string) => {
   return map.filter(t => t === '#').length;
 };
 
-const slope = (a: point, b: point): number => {
-  if (a.x == b.x) {
-    return 0;
-  }
-
-  return (b.y - a.y) / (b.x - a.x);
-}
-
-const intercept = (point: point, slope: number): number => {
-  if (slope === null) {
-    // vertical line
-    return point.x;
-  }
-
-  return point.y - slope * point.x;
-}
-
-const line = (start: point, end: point): point[] => {
-  var m = slope(start, end);
-  var b = intercept(start, m);
-
-  const coordinates: point[] = [];
-  for (var x = Math.min(start.x, end.x); x <= Math.max(start.x, end.x); x++) {
-    var y = m * x + b;
-    coordinates.push({ x, y });
-  }
-
-  return coordinates;
-}
-
-type scan = { id: number, topToRight: point[], rightToBottom: point[], bottomToLeft: point[], leftToTop: point[], center: point, beacon: point, distance: number };
+type scan = {
+  id: number,
+  topToRightInner: point[],
+  rightToBottomInner: point[],
+  bottomToLeftInner: point[],
+  leftToTopInner: point[],
+  topToRightOuter: point[],
+  rightToBottomOuter: point[],
+  bottomToLeftOuter: point[],
+  leftToTopOuter: point[],
+  center: point,
+  beacon: point,
+  distance: number
+};
 
 const part2 = (rawInput: string) => {
   const offset = 0;
+  const limits: point = { x: 20, y: 20 };
+
+  const slope = (a: point, b: point): number => {
+    if (a.x == b.x) {
+      return 0;
+    }
+
+    return (b.y - a.y) / (b.x - a.x);
+  }
+
+  const intercept = (point: point, slope: number): number => {
+    if (slope === null) {
+      // vertical line
+      return point.x;
+    }
+
+    return point.y - slope * point.x;
+  }
+
+  const line = (start: point, end: point): point[] => {
+    const m = slope(start, end);
+    const b = intercept(start, m);
+
+    const coordinates: point[] = [];
+    for (let x = Math.min(start.x, end.x); x <= Math.max(start.x, end.x); x++) {
+      const y = m * x + b;
+      if (x >= 0 && x < limits.x && y >= 0 && y < limits.y) {
+        coordinates.push({ x, y });
+      }
+    }
+
+    return coordinates;
+  }
 
   const parseInput = (rawInput: string): scan[] => rawInput.split('\n').map((l, i) => {
     const pointData = l.replace(/x=/g, '').replace(/y=/g, '').replace('Sensor at ', '').replace(' closest beacon is at ', '');
-    const points = pointData.split(':').map((p) => {
+    const points: point[] = pointData.split(':').map((p) => {
       const point = p.split(',').map(v => parseInt(v, 10));
       return { x: point[0] + offset, y: point[1] + offset };
     });
 
     const distance = dist(points[0], points[1]);
-    const top = { x: points[0].x, y: points[0].y - distance - 1 };
-    const right = { x: points[0].x + distance + 1, y: points[0].y };
-    const bottom = { x: points[0].x, y: points[0].y + distance + 1 };
-    const left = { x: points[0].x - distance - 1, y: points[0].y };
+
+    const topInner = { x: points[0].x, y: points[0].y - distance };
+    const rightInner = { x: points[0].x + distance, y: points[0].y };
+    const bottomInner = { x: points[0].x, y: points[0].y + distance };
+    const leftInner = { x: points[0].x - distance, y: points[0].y };
+
+    const topOuter = { x: points[0].x, y: points[0].y - distance - 1 };
+    const rightOuter = { x: points[0].x + distance + 1, y: points[0].y };
+    const bottomOuter = { x: points[0].x, y: points[0].y + distance + 1 };
+    const leftOuter = { x: points[0].x - distance - 1, y: points[0].y };
 
     return {
       id: i,
-      topToRight: line(top, right),
-      rightToBottom: line(right, bottom),
-      bottomToLeft: line(bottom, left),
-      leftToTop: line(left, top),
+      topToRightInner: line(topInner, rightInner),
+      rightToBottomInner: line(rightInner, bottomInner),
+      bottomToLeftInner: line(bottomInner, leftInner),
+      leftToTopInner: line(leftInner, topInner),
+
+      topToRightOuter: line(topOuter, rightOuter),
+      rightToBottomOuter: line(rightOuter, bottomOuter),
+      bottomToLeftOuter: line(bottomOuter, leftOuter),
+      leftToTopOuter: line(leftOuter, topOuter),
       center: points[0],
       beacon: points[1],
       distance
@@ -111,7 +138,6 @@ const part2 = (rawInput: string) => {
   });
 
   const scans = parseInput(rawInput);
-  const limits: point = { x: 20, y: 20 };
 
   const drawLine = (points: point[]) => {
     for (const point of points) {
@@ -134,23 +160,6 @@ const part2 = (rawInput: string) => {
     return map;
   }
 
-  const findFreePoint = (points: point[], scanId: number): point | null => {
-    for (const pointToFind of points) {
-      const isOnAnyLine = scans
-        .filter(scan => scan.id !== scanId)
-        .some(scan =>
-          scan.topToRight.some(p => p.x === pointToFind.x && p.y === pointToFind.y)
-          || scan.rightToBottom.some(p => p.x === pointToFind.x && p.y === pointToFind.y)
-          || scan.bottomToLeft.some(p => p.x === pointToFind.x && p.y === pointToFind.y)
-          || scan.leftToTop.some(p => p.x === pointToFind.x && p.y === pointToFind.y));
-
-      if (isOnAnyLine) return null;
-      return pointToFind;
-    }
-
-    return null
-  }
-
   const map: string[][] = [];
   for (let y = 0; y < limits.y; y++) {
     map.push(''.padEnd(limits.x, '.').split(''));
@@ -158,22 +167,12 @@ const part2 = (rawInput: string) => {
 
   // fs.writeFileSync('./src/day15/map.txt', JSON.stringify(scans));
 
+  const pointsOnEdgesOfScans = scans.flatMap(s => s.topToRightInner.concat(s.rightToBottomInner, s.bottomToLeftInner, s.leftToTopInner));
+  const pointOutsideOfScans = scans.flatMap(s => s.topToRightOuter.concat(s.rightToBottomOuter, s.bottomToLeftOuter, s.leftToTopOuter));
+
+  const freePoints = pointOutsideOfScans.filter(p => pointsOnEdgesOfScans .includes(p));
   let freePoint = null;
-  for (const scan of scans) {
-    // drawLine(scan.topToRight);
-    // drawLine(scan.rightToBottom);
-    // drawLine(scan.bottomToLeft);
-    // drawLine(scan.leftToTop);
-    drawScan(scan);
-    freePoint = findFreePoint(scan.topToRight, scan.id);
-    if (freePoint) break;
-    freePoint = findFreePoint(scan.rightToBottom, scan.id);
-    if (freePoint) break;
-    freePoint = findFreePoint(scan.bottomToLeft, scan.id);
-    if (freePoint) break;
-    freePoint = findFreePoint(scan.leftToTop, scan.id);
-    if (freePoint) break;
-  }
+  if (freePoints.length > 0) freePoint = freePoints[0];
 
   console.log('free point found?', freePoint);
   fs.writeFileSync('./src/day15/map.txt', map.map(row => row.join('')).join('\n'));
