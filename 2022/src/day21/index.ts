e5f4128207c3eee5f7acc6a304e2639e016cdcd5
Martijn Kooij <martijn.kooij@snappet.org>
2022-12-21 13:30:15 +0100

    Approximating part 2

--- 2022/src/day21/index.ts
+++ 2022/src/day21/index.ts
@@ -10,11 +10,12 @@ const operators: { [id: string]: (a: number, b: number) => number } = {
 class Monkey {
   public value: number = 0;
   public monkeyId1: string = '';
-  public operation: string = '';
-  public monkeyId2: string = '';
+  public monkey1: Monkey | undefined;
 
-  private monkey1: Monkey | undefined;
-  private monkey2: Monkey | undefined;
+  public operation: string = '';
+
+  public monkeyId2: string = '';
+  public monkey2: Monkey | undefined;
 
   public get isMathMonkey() {
     return this.monkeyId1 && this.operation && this.monkeyId2;
@@ -27,6 +28,10 @@ class Monkey {
 
     this.monkey1 = monkeys.find(m => m.id === this.monkeyId1);
     this.monkey2 = monkeys.find(m => m.id === this.monkeyId2);
+  }
+
+  public reset() {
+    if (this.isMathMonkey) this.value = 0;
   }
 
   public execute(): boolean {
@@ -59,8 +64,24 @@ const part1 = (rawInput: string) => {
   const monkeys = parseInput(rawInput);
   monkeys.forEach(m => m.setRequiredMonkeys(monkeys));
 
+  const human = monkeys.find(m => m.id === 'humn');
+  if (human) {
+    human.value = 3715799488132;
+  }
   while (!monkeys.every(m => !!m.value)) {
     monkeys.forEach(m => m.execute());
+  }
+
+  /*
+  human value
+  94625185243550.00
+  94625185243550.00
+  */
+
+  const round = (num: number) => Math.abs(num).toFixed(2);
+  const root = monkeys.find(m => m.id === 'root');
+  if (root) {
+    console.log('root values', round((root.monkey1?.value ?? 0)), round((root.monkey2?.value ?? 0)), round((root.monkey1?.value ?? 0)) < round((root.monkey2?.value ?? 0)) ? 'make human SMALLER' : 'make human BIGGER');
   }
 
   return monkeys.find(m => m.id === 'root')?.value;
@@ -70,38 +91,76 @@ const part2 = (rawInput: string) => {
   const monkeys = parseInput(rawInput);
   monkeys.forEach(m => m.setRequiredMonkeys(monkeys));
 
-  while (!monkeys.every(m => !!m.value)) {
-    monkeys.forEach((m) => {
-      if (m.id === 'root')
-      m.execute();
-    });
+  const human = monkeys.find(m => m.id === 'humn');
+  if (!human) throw new Error('No human found...');
+
+  let found = false;
+  let humanValue = 3715799489000;
+  let escape = 0;
+  while (!found) {
+    escape++;
+    if (escape > 100) {
+      console.log('nope...');
+      break;
+    }
+    monkeys.forEach(m => m.reset());
+    humanValue++;
+    human.value = humanValue;
+
+    const findRoot = (): boolean => {
+      let innerEscape = 0;
+      let found = false;
+
+      while (true) {
+        if (found || monkeys.every(m => !!m.value)) return found;
+  
+        innerEscape++;
+        if (innerEscape > 33) {
+          console.log('inner nope...', innerEscape);
+          break;
+        }
+        monkeys.forEach((m) => {
+          if (m.id === 'root') {
+            found = !!m.monkey1?.value && !!m.monkey2?.value && m.monkey1?.value === m.monkey2?.value;
+            if (found) console.log('root matches!', m.monkey1?.value, m.monkey2?.value, found, humanValue);
+          }
+          m.execute();
+        });
+      }
+
+      return found;
+    }
+
+    found = findRoot();
   }
 
-  return monkeys.find(m => m.id === 'root')?.value;
+  // console.log('monkeys', monkeys);
+
+  return found ? humanValue : 'NOT FOUND!';
 };
 
 run({
   part1: {
     tests: [
-      {
-        input: `
-        root: pppw + sjmn
-        dbpl: 5
-        cczh: sllz + lgvd
-        zczc: 2
-        ptdq: humn - dvpt
-        dvpt: 3
-        lfqf: 4
-        humn: 5
-        ljgn: 2
-        sjmn: drzm * dbpl
-        sllz: 4
-        pppw: cczh / lfqf
-        lgvd: ljgn * ptdq
-        drzm: hmdt - zczc
-        hmdt: 32`,
-        expected: 152,
-      },
+      // {
+      //   input: `
+      //   root: pppw + sjmn
+      //   dbpl: 5
+      //   cczh: sllz + lgvd
+      //   zczc: 2
+      //   ptdq: humn - dvpt
+      //   dvpt: 3
+      //   lfqf: 4
+      //   humn: 5
+      //   ljgn: 2
+      //   sjmn: drzm * dbpl
+      //   sllz: 4
+      //   pppw: cczh / lfqf
+      //   lgvd: ljgn * ptdq
+      //   drzm: hmdt - zczc
+      //   hmdt: 32`,
+      //   expected: 152,
+      // },
     ],
     solution: part1,
   },
@@ -130,5 +189,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
