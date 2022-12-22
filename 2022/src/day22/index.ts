dc82d0db7d1d799f84e7bf6e18d2ae9063b37c2e
Martijn Kooij <martijn.kooij@snappet.org>
2022-12-22 15:44:08 +0100

    Day 22 WiP

--- 2022/src/day22/index.ts
+++ 2022/src/day22/index.ts
@@ -1,7 +1,10 @@
 import run from 'aocrunner';
 import * as fs from 'fs';
 
-const parseInput = (rawInput: string) => {
+type input = { moves: string[], map: string[][] };
+type point = { x: number, y: number };
+
+const parseInput = (rawInput: string): input => {
   const blocks = rawInput.split('\n\n');
 
   return {
@@ -10,28 +13,51 @@ const parseInput = (rawInput: string) => {
   }
 };
 
-
-const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-
+const offset = 1;
+const drawMap = (data: input) => {
   let maxX = 0;
   let maxY = 0;
-  for (let y = 0; y < input.map.length; y++) {
-    for (let x = 0; x < input.map[y].length; x++) {
+  for (let y = 0; y < data.map.length; y++) {
+    for (let x = 0; x < data.map[y].length; x++) {
       if (x > maxX) maxX = x;
       if (y > maxY) maxY = y;
     }
   }
 
   const map: string[][] = [];
-  for (let y = 0; y < maxY + 1; y++) {
-    map.push(''.padEnd(maxX + 1, ' ').split(''));
+  for (let y = 0; y < maxY + 3; y++) {
+    map.push(''.padEnd(maxX + 3, ' ').split(''));
   }
 
-  for (let y = 0; y < input.map.length; y++) {
-    for (let x = 0; x < input.map[y].length; x++) {
-      map[y][x] = input.map[y][x];      
-    }    
+  for (let y = 0; y < data.map.length; y++) {
+    for (let x = 0; x < data.map[y].length; x++) {
+      map[y + offset][x + offset] = data.map[y][x];
+    }
+  }
+
+  return { map, maxX: maxX + offset, maxY: maxY + offset };
+}
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  const { map, maxX, maxY } = drawMap(input);
+
+  const velocity: point = { x: 0, y: 0 };
+  const position: point = { x: map[offset].findIndex(p => p === '.'), y: offset };
+
+  console.log('start', position);
+
+  for (const move of input.moves) {
+    const moves = Number(move);
+    if (isNaN(moves)) {
+
+    } else {
+      for (let m = 0; m < moves; m++) {
+        let newX = position.x + velocity.x;
+        let newY = position.y + velocity.y;
+        if (map[newY])
+      }
+    }
   }
 
   fs.writeFileSync('./src/day22/map.txt', map.map(row => row.join('')).join('\n'));
