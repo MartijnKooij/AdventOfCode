4711d6b022ec475c4c62b385109a84f75c6fe30b
Martijn Kooij <post@martijnkooij.nl>
2022-12-24 00:06:21 +0100

    Day 22.2 WiP

--- 2022/src/day22/index.ts
+++ 2022/src/day22/index.ts
@@ -141,6 +141,8 @@ const part1 = (rawInput: string) => {
 /* ######### PART 2 ############
 /* ############################# */
 
+type cube = { id: number, mapStart: point, map: string[][] };
+
 const part2 = (rawInput: string) => {
   const parseInput = (rawInput: string): input => {
     const blocks = rawInput.split('\n\n');
@@ -179,23 +181,152 @@ const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
   const { map, maxX, maxY } = drawMap(input);
 
-  const cubes = Array.from(Array(4), () => Array.from(Array(4), () => new Array()));
-  const cubeIds = new Set();
-  for (let y = 0; y < map.length; y++) {
-    const cubeY = Math.floor(y / 3);
-    for (let x = 0; x < map[y].length; x++) {
-      const cubeX = Math.floor(x / 4);
-      if (map[y][x] === ' ') continue;
-      cubeIds.add(cubeX + ',' + cubeY);
-      console.log(cubeX, cubeY, x, y, map[y][x]);
-      cubes[cubeY][cubeX].push(map[y][x]);
-    }
+  const faceSize = 4;
 
+  const cubeMap: string[][] = [];
+  for (let y = 0; y < faceSize; y++) {
+    cubeMap.push(''.padEnd(faceSize, ' ').split(''));
   }
 
-  console.log(cubeIds);
+  const cubes: cube[] = [
+    { id: 0, mapStart: { x: 404, y: 404 }, map: cubeMap }, // cube 0 does not exist...
+    { id: 1, mapStart: { x: 8, y: 0 }, map: JSON.parse(JSON.stringify(cubeMap)) },
+    { id: 2, mapStart: { x: 0, y: 4 }, map: JSON.parse(JSON.stringify(cubeMap)) },
+    { id: 3, mapStart: { x: 4, y: 4 }, map: JSON.parse(JSON.stringify(cubeMap)) },
+    { id: 4, mapStart: { x: 8, y: 4 }, map: JSON.parse(JSON.stringify(cubeMap)) },
+    { id: 5, mapStart: { x: 8, y: 8 }, map: JSON.parse(JSON.stringify(cubeMap)) },
+    { id: 6, mapStart: { x: 12, y: 8 }, map: JSON.parse(JSON.stringify(cubeMap)) }
+  ];
 
-  return;
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      for (const cube of cubes) {
+        if (x >= cube.mapStart.x && x < cube.mapStart.x + faceSize &&
+          y >= cube.mapStart.y && y < cube.mapStart.y + faceSize) {
+          cube.map[y - cube.mapStart.y][x - cube.mapStart.x] = map[y][x];
+        }
+      }
+    }
+  }
+
+  //fs.writeFileSync('./src/day22/map.txt', cubes.slice(1).map(cube => cube.map.map(row => row.join('')).join('\n')).join('\n\n'));
+
+  const moveHorizontal = (map: string[][], currentPosition: point, count: number, direction: move): point => {
+    for (let m = 0; m < count; m++) {
+      let newPosition = { x: currentPosition.x + (direction === '>' ? 1 : -1), y: currentPosition.y };
+      if (map[newPosition.y][newPosition.x] === ' ') {
+        newPosition = direction === '>' ? findNextXWhenExitOnTheRight(map, currentPosition) : findNextXWhenExitOnTheLeft(map, currentPosition);
+        if (map[newPosition.y][newPosition.x] !== '#') {
+          currentPosition = newPosition;
+        }
+      } else if (map[newPosition.y][newPosition.x] !== '#') {
+        currentPosition = newPosition;
+      }
+      map[currentPosition.y][currentPosition.x] = direction;
+    }
+
+    return currentPosition;
+  }
+
+  const isInCubeFace = (cubeIndex: number, position: point): boolean => {
+    return position.x >= cubes[cubeIndex].mapStart.x && position.x < cubes[cubeIndex].mapStart.x + faceSize &&
+      position.y >= cubes[cubeIndex].mapStart.y && position.y < cubes[cubeIndex].mapStart.y + faceSize
+  }
+
+  const findNextXWhenExitOnTheLeft = (map: string[][], currentPosition: point): point => {
+    // Normal wrap around
+    return { y: currentPosition.y, x: map[currentPosition.y].findLastIndex(p => p !== ' ') };
+  }
+
+  const findNextXWhenExitOnTheRight = (map: string[][], currentPosition: point): point => {
+    if (isInCubeFace(1, currentPosition)) {
+      // 1 > 6, moving <, invert y
+      return { x: cubes[6].mapStart.x + faceSize - 1, y: faceSize - Math.floor((currentPosition.y + 1) / faceSize) };
+    } else if (isInCubeFace(4, currentPosition)) {
+      // 4 > 6, moving v, x becomes y offset
+      console.log('Leaving 4 v 6?', Math.floor((currentPosition.y) / faceSize), { x: cubes[6].mapStart.x + faceSize - Math.floor((currentPosition.y + 1) / faceSize), y: cubes[6].mapStart.y });
+      return { x: cubes[6].mapStart.x + Math.floor((currentPosition.y) / faceSize), y: cubes[6].mapStart.y };
+    }
+
+    // Normal wrap around
+    return { y: currentPosition.y, x: map[currentPosition.y].findIndex(p => p !== ' ') };
+  }
+
+  const findNextYWhenExitOnTheBottom = (map: string[][], x: number): number => {
+    for (let y = 0; y < map.length; y++) {
+      if (map[y][x] !== ' ') return y;
+    }
+    return -1;
+  }
+
+  const findNextYWhenExitOnTheTop = (map: string[][], x: number): number => {
+    for (let y = map.length - 1; y >= 0; y--) {
+      if (map[y][x] !== ' ') return y;
+    }
+    return -1;
+  }
+
+  const moveVertical = (map: string[][], position: point, count: number, direction: move): point => {
+    for (let m = 0; m < count; m++) {
+      let newY = position.y + (direction === '^' ? -1 : 1);
+      if (map[newY][position.x] === ' ') {
+        newY = direction === '^' ? findNextYWhenExitOnTheTop(map, position.x) : findNextYWhenExitOnTheBottom(map, position.x);
+        if (map[newY][position.x] !== '#') {
+          position.y = newY;
+        }
+      } else if (map[newY][position.x] !== '#') {
+        position.y = newY;
+      }
+      map[position.y][position.x] = direction;
+    }
+
+    return position;
+  }
+
+  let position: point = { x: map[offset].findIndex(p => p === '.'), y: offset };
+
+  console.log('start', position, input.moves);
+
+  let currentDirection = 0;
+  const availableMoves: move[] = ['>', 'v', '<', '^'];
+  map[position.y][position.x] = availableMoves[currentDirection];
+  for (const move of input.moves) {
+    const moves = Number(move);
+    console.log('Checking move', move, moves);
+
+    if (isNaN(moves)) {
+      currentDirection += move === 'R' ? 1 : -1;
+      if (currentDirection < 0) currentDirection = availableMoves.length - 1;
+      if (currentDirection >= availableMoves.length) currentDirection = 0;
+      map[position.y][position.x] = availableMoves[currentDirection];
+
+      // console.log('Turning', move, availableMoves[currentDirection]);
+    } else {
+      switch (availableMoves[currentDirection]) {
+        case '>':
+        case '<':
+          // console.log('Moving horizontally', position, moves);
+          position = moveHorizontal(map, position, moves, availableMoves[currentDirection]);
+          // console.log('Moved horizontally', position, moves);
+          break;
+        case '^':
+        case 'v':
+          // console.log('Moving vertically', position, moves);
+          position = moveVertical(map, position, moves, availableMoves[currentDirection]);
+          // console.log('Moved vertically', position, moves);
+          break;
+        default:
+          break;
+      }
+    }
+    map[position.y][position.x] = availableMoves[currentDirection];
+  }
+
+  console.log('Done', position, availableMoves[currentDirection]);
+  fs.writeFileSync('./src/day22/log.txt', JSON.stringify(input.moves));
+  fs.writeFileSync('./src/day22/map.txt', map.map(row => row.join('')).join('\n'));
+
+  return ((position.y) * 1000) + ((position.x) * 4) + currentDirection;
 };
 
 run({
