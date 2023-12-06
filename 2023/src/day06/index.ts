e9b9d22a6e0b717449f5da8cc7734275dfa4af62
Martijn Kooij <martijn.kooij@snappet.org>
2023-12-06 11:19:11 +0100

    Day 6

--- 2023/src/day06/index.ts
+++ 2023/src/day06/index.ts
@@ -8,15 +8,43 @@ const part1 = (rawInput: string) => {
   const distances = input[1].substring(9).split(' ').filter(v => !!v).map(Number);
   const distance = (pressed: number, time: number) => pressed * (time - pressed);
 
-  console.log('data', times, distances, distance(4, 7));
+  console.log('data', times, distances);
 
-  return;
+  const waysToWin = [];
+  for (let t = 0; t < times.length; t++) {
+    const time = times[t];
+    const shortestTimes = [];
+    for (let p = 0; p < time; p++) {
+      const d = distance(p, time);
+      if (d > distances[t]) {
+        shortestTimes.push(p);
+      }
+    }
+    waysToWin.push(shortestTimes.length);
+  }
+
+  return waysToWin.reduce((p, c) => p * c, 1);
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const time = Number(input[0].substring(5).replaceAll(' ', ''));
+  const distanceToBeat = Number(input[1].substring(9).replaceAll(' ', ''));
+  const distance = (pressed: number, time: number) => pressed * (time - pressed);
 
-  return;
+  console.log('data', time, distanceToBeat);
+
+  const waysToWin = [];
+  const shortestTimes = [];
+  for (let p = 0; p < time; p++) {
+    const d = distance(p, time);
+    if (d > distanceToBeat) {
+      shortestTimes.push(p);
+    }
+  }
+  waysToWin.push(shortestTimes.length);
+
+  return waysToWin.reduce((p, c) => p * c, 1);
 };
 
 run({
@@ -33,13 +61,15 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        Time:      7  15   30
+        Distance:  9  40  200`,
+        expected: 71503,
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
