2a94f8bf25fbea261af681c1142451c0fb7f7961
Martijn Kooij <martijn.kooij@snappet.org>
2023-12-17 00:10:03 +0100

    Cleaning up

--- 2023/README.md
+++ 2023/README.md
@@ -26,7 +26,7 @@
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
 [![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%86/yellow)](src/day14)
 [![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%85/green)](src/day15)
-![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/16/%E2%98%85%E2%98%86/yellow)](src/day16)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/19/%E2%98%86%E2%98%86/gray)
@@ -160,9 +160,9 @@ Both parts: 39.633ms
 
 ```
 Day 14
-Time part 1: 2.602ms
+Time part 1: 8.769ms
 Time part 2: -
-Both parts: 2.602ms
+Both parts: 8.769ms
 ```
 
 ```
@@ -174,9 +174,9 @@ Both parts: 8.267ms
 
 ```
 Day 16
-Time part 1: -
+Time part 1: 213.783ms
 Time part 2: -
-Both parts: -
+Both parts: 213.783ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 28/50
-Total time: 2644104.959ms
+Total stars: 29/50
+Total time: 2644324.91ms
 ```
 
 <!--/RESULTS-->
--- 2023/src/day16/index.ts
+++ 2023/src/day16/index.ts
@@ -2,7 +2,7 @@ import run from 'aocrunner';
 import * as fs from 'fs';
 
 class Node {
-  public energized: string[] = [];
+  public energized: boolean = false;
   constructor(public sign: string) { }
 }
 
@@ -41,61 +41,62 @@ const part1 = (rawInput: string) => {
 
   fs.writeFileSync('./map.txt', map.map(l => l
     .map(n =>
-      // n.energized.length > 0 ? n.energized[0] : n.sign
-      n.energized.length > 0 && n.sign === '.'
-        ? ([...new Set(n.energized)].length === 1
-          ? n.energized[0]
-          : [...new Set(n.energized)].length)
-        : n.sign
+      n.energized ? '#' : n.sign
     ).join('')).join('\n'));
 
-  const result =  map.flatMap(l => l.flatMap(n => n.energized.length > 0));
+  const result = map.flatMap(l => l.flatMap(n => n.energized));
 
   return result.filter(n => !!n).length;
 };
 
-function followBeam(map: Node[][], beam: string, x: number, y: number, depth: number = 0) {
+function followBeam(map: Node[][], beam: string, x: number, y: number, checked: Direction[] = []) {
   let escape = 0;
   let newX = x;
   let newY = y;
-  let debug = false;
-  let rnd = Math.random();
+  // let debug = false;
+  // let rnd = Math.random();
   while (true) {
-    if (newY === 6 && newX === 7) {
-      // debug = true;
-    }
-    if (debug) {
-      console.log('TOP OF THE LOOP', rnd, newX, newY, beam);
-    }
+    // if (newY === 6 && newX === 7) {
+    //   debug = true;
+    // }
+    // if (debug) {
+    //   console.log('TOP OF THE LOOP', rnd, newX, newY, beam);
+    // }
     if (newX < 0 || newX >= map[0].length || newY < 0 || newY >= map.length) {
       // if (debug) {
       //   console.log('breaking off...');
       // }
       break;
     }
-    if (depth > 100) {
-      // console.log('Too deep', rnd);
+    if (checked.some(d => d.x === newX && d.y === newY && d.beam === beam)) {
+      // Already did this route.
       break;
     }
+    checked.push({ x: newX, y: newY, beam });
+    escape++;
+    if (escape > 1000000) {
+      break;
+    }
+
     const currentNode = map[newY][newX];
-    if (debug) {
-      console.log('currentNode', rnd, currentNode);
-    }
+    // if (debug) {
+    //   console.log('currentNode', rnd, currentNode);
+    // }
 
-    currentNode.energized.push(beam);
+    currentNode.energized = true;
     const command = directions.get(beam + currentNode.sign)!;
-    if (!command) {
-      debug = true;
-    }
+    // if (!command) {
+    //   debug = true;
+    // }
 
-    if (debug) {
-      console.log('beam', rnd, beam);
-      console.log('sign', map[newY][newX].sign);
-      console.log('command', command);
-    }
+    // if (debug) {
+    //   console.log('beam', rnd, beam);
+    //   console.log('sign', map[newY][newX].sign);
+    //   console.log('command', command);
+    // }
     if (command.split) {
-      followBeam(map, command.beam.substring(0, 1), newX - command.x, newY - command.y, ++depth);
-      followBeam(map, command.beam.substring(1, 2), newX + command.x, newY + command.y, ++depth);
+      followBeam(map, command.beam.substring(0, 1), newX - command.x, newY - command.y, checked);
+      followBeam(map, command.beam.substring(1, 2), newX + command.x, newY + command.y, checked);
       break;
     }
 
@@ -103,14 +104,9 @@ function followBeam(map: Node[][], beam: string, x: number, y: number, depth: nu
     newY = newY + command.y;
     beam = command.beam
 
-    if (debug) {
-      console.log('debug next', newX, newY, beam);
-    }
-
-    escape++;
-    if (escape > 100000) {
-      break;
-    }
+    // if (debug) {
+    //   console.log('debug next', newX, newY, beam);
+    // }
   }
 }
 
