96627dc172b8a0808bedd31ff55eff3440c87d26
Martijn Kooij <post@martijnkooij.nl>
2024-12-01 11:26:43 +0100

    Day 1

--- 2024/src/day01/index.ts
+++ 2024/src/day01/index.ts
@@ -1,38 +1,68 @@
 import run from 'aocrunner';
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) => rawInput.split('\n').map(l => l.split(/\s+/).map(s => Number(s)));
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const left = input.map(l => l[0]).sort();
+  const right = input.map(l => l[1]).sort();
+  const diffs = [];
 
-  return;
+  for (let i = 0; i < left.length; i++) {
+    diffs.push(Math.max(left[i], right[i]) - Math.min(left[i], right[i]));
+  }
+
+  return diffs.reduce((a, b) => a + b, 0);
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const left = input.map(l => l[0]);
+  const right = input.map(l => l[1]);
+  const totals = [];
 
-  return;
+  for (let i = 0; i < left.length; i++) {
+    const num = left[i];
+    const occ = right.reduce((count, current) => (current === num ? count + 1 : count), 0);
+    totals.push(occ * num);
+  }
+
+  return totals.reduce((a, b) => a + b, 0);
 };
 
 run({
   part1: {
     tests: [
       {
-        input: ``,
-        expected: "",
+        input: `
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+        `,
+        expected: 11,
       },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+        `,
+        expected: 31,
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
