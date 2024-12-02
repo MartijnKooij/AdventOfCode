663f6d382d27b593f85fda0594c3271312987c91
Martijn Kooij <martijn.kooij@snappet.org>
2024-12-02 10:01:58 +0100

    Day 2 complete

--- 2024/src/day02/index.ts
+++ 2024/src/day02/index.ts
@@ -2,36 +2,24 @@ import run from 'aocrunner';
 
 const parseInput = (rawInput: string) => rawInput.split("\n").map((line) => line.split(" ").map(Number));
 
-const testReport = (report: number[], retry = false, log = false) => {
+const testReport = (report: number[], log = false) => {
   let allSafe = true;
   let direction = 0;
 
   for (let i = 0; i < report.length - 1; i++) {
     const diff = report[i + 1] - report[i];
     if (Math.abs(diff) > 3 || diff === 0) {
-      if (retry) {
-        report.splice(i, 1);
-        return testReport(report, false);
-      }
       allSafe = false;
       break;
     }
     if (diff > 0) {
       if (direction < 0) {
-        if (retry) {
-          report.splice(i, 1);
-          return testReport(report, false);
-        }
         allSafe = false;
         break;
       }
       direction = 1;
     } else if (diff < 0) {
       if (direction > 0) {
-        if (retry) {
-          report.splice(i, 1);
-          return testReport(report, false);
-        }
         allSafe = false;
         break;
       }
@@ -41,12 +29,13 @@ const testReport = (report: number[], retry = false, log = false) => {
 
   if (allSafe) {
     if (log) {
-      // console.log('safe?', report);
+      console.log('safe?', report.join(' '));
     }
     return true;
   }
+
   if (log) {
-    console.log('unsafe?', report);
+    console.log('unsafe?', report.join(' '));
   }
   return false;
 };
@@ -67,8 +56,21 @@ const part2 = (rawInput: string) => {
   const reports = parseInput(rawInput);
   let safeReports = 0;
   for (const report of reports) {
-    if (testReport(report, true, true)) {
+    let isSafe = false;
+    console.log('testing', report);
+    if (testReport(report, true)) {
       safeReports++;
+      isSafe = true;
+    }
+    if (!isSafe) {
+      for (let i = 0; i < report.length; i++) {
+        const currentReport = [...report];
+        currentReport.splice(i, 1);
+        if (testReport(currentReport, true)) {
+          safeReports++;
+          break;
+        }
+      }
     }
   }
 
@@ -98,8 +100,7 @@ run({
 9 7 6 2 1
 1 3 2 4 5
 8 6 4 4 1
-1 3 6 7 9`,
-        expected: 4,
+1 3 6 7 9`, expected: 4,
       },
     ],
     solution: part2,
