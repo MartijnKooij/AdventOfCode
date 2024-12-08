84601585b717b7fe652bd2c9be1fb974faf51bcc
Martijn Kooij <post@martijnkooij.nl>
2024-12-08 11:54:30 +0100

    Day 8 p1

---
+++ 2024/src/day08/index.ts
@@ -0,0 +1,88 @@
+import run from 'aocrunner';
+import { AocMap } from '../utils/map.js';
+
+type Point = { x: number; y: number };
+const parseInput = (rawInput: string) => new AocMap(rawInput);
+const findOtherAntennas = (map: AocMap, antennaX: number, antennaY: number) => {
+  const antenna = map.values[antennaY][antennaX];
+  const otherAntennas: Point[] = [];
+  for (let y = 0; y < map.rows; y++) {
+    for (let x = 0; x < map.columns; x++) {
+      const cell = map.values[y][x];
+      if (cell === antenna && x !== antennaX && y !== antennaY) {
+        otherAntennas.push({ x, y });
+      }
+    }
+  }
+  return otherAntennas;
+};
+
+const part1 = (rawInput: string) => {
+  const map = parseInput(rawInput);
+  let antiNodes: Set<string> = new Set();
+  for (let y = 0; y < map.rows; y++) {
+    for (let x = 0; x < map.columns; x++) {
+      const cell = map.values[y][x];
+      if (cell !== '.' && cell !== '#') {
+        const otherAntennas = findOtherAntennas(map, x, y);
+        for (const { x: otherX, y: otherY } of otherAntennas) {
+          const diffX = otherX - x;
+          const diffY = otherY - y;
+          const antiNode1 = { x: x - diffX, y: y - diffY };
+          const antiNode2 = { x: otherX + diffX, y: otherY + diffY };
+          if (antiNode1.x >= 0 && antiNode1.x < map.columns && antiNode1.y >= 0 && antiNode1.y < map.rows) {
+            // map.values[antiNode1.y][antiNode1.x] = '#';
+            antiNodes.add(`${antiNode1.x},${antiNode1.y}`);
+          }
+          if (antiNode2.x >= 0 && antiNode2.x < map.columns && antiNode2.y >= 0 && antiNode2.y < map.rows) {
+            // map.values[antiNode2.y][antiNode2.x] = '#';
+            antiNodes.add(`${antiNode2.x},${antiNode2.y}`);
+          }
+        }
+      }
+    }
+  }
+
+  return antiNodes.size;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+`,
+        expected: 14,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
