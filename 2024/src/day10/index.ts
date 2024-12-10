47609e3253e86635af77e31c6e5edf0a38db3f09
Martijn Kooij <post@martijnkooij.nl>
2024-12-10 13:48:25 +0100

    Day 10

--- 2024/src/day10/index.ts
+++ 2024/src/day10/index.ts
@@ -12,6 +12,50 @@ const directions = [
 
 const countTrails = (input: AocMap, startLocation: Point, visited: Set<string>): number => {
   const currentHeight = Number(input.get(startLocation.x, startLocation.y));
+  const currentLocationKey = `${startLocation.x},${startLocation.y}${currentHeight}`;
+  if (currentHeight === 9) {
+    if (!visited.has(currentLocationKey)) {
+      visited.add(currentLocationKey);
+      return 1;
+    }
+    visited.add(currentLocationKey);
+    return 0;
+  }
+
+  visited.add(currentLocationKey);
+
+  let pathCount = 0;
+
+  for (const direction of directions) {
+    const nextLocation = {
+      x: startLocation.x + direction.x,
+      y: startLocation.y + direction.y,
+    };
+    if (
+      nextLocation.x < 0 ||
+      nextLocation.x >= input.columns ||
+      nextLocation.y < 0 ||
+      nextLocation.y >= input.rows
+    ) {
+      continue;
+    }
+
+    const nextHeight = Number(input.get(nextLocation.x, nextLocation.y));
+    if (nextHeight - currentHeight !== 1) {
+      continue;
+    }
+
+    const nextLocationKey = `${nextLocation.x},${nextLocation.y}${nextHeight}`;
+    if (!visited.has(nextLocationKey)) {
+      pathCount += countTrails(input, nextLocation, visited);
+    }
+  }
+
+  return pathCount;
+};
+
+const countTrails2 = (input: AocMap, startLocation: Point, visited: Set<string>): number => {
+  const currentHeight = Number(input.get(startLocation.x, startLocation.y));
   if (currentHeight === 9) {
     return 1;
   }
@@ -40,7 +84,7 @@ const countTrails = (input: AocMap, startLocation: Point, visited: Set<string>):
 
     const nextLocationKey = `${nextLocation.x},${nextLocation.y}`;
     if (!visited.has(nextLocationKey)) {
-      count += countTrails(input, nextLocation, new Set(visited));
+      count += countTrails2(input, nextLocation, new Set(visited));
     }
   }
 
@@ -61,7 +105,7 @@ const part1 = (rawInput: string) => {
   let sumOfTrails = 0
   for (const startLocation of startLocations) {
     let sumOfTrail = countTrails(input, startLocation, new Set());
-    console.log("sumOfTrail", sumOfTrail);
+    // console.log("sumOfTrail", sumOfTrail);
     sumOfTrails += sumOfTrail;
   }
 
@@ -69,9 +113,24 @@ const part1 = (rawInput: string) => {
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const input: AocMap = parseInput(rawInput);
+  const startLocations: Point[] = [];
+  for (let y = 0; y < input.rows; y++) {
+    for (let x = 0; x < input.columns; x++) {
+      if (input.get(x, y) === "0") {
+        startLocations.push({ x, y });
+      }
+    }
+  }
 
-  return;
+  let sumOfTrails = 0
+  for (const startLocation of startLocations) {
+    let sumOfTrail = countTrails2(input, startLocation, new Set());
+    console.log("sumOfTrail", sumOfTrail);
+    sumOfTrails += sumOfTrail;
+  }
+
+  return sumOfTrails;
 };
 
 run({
@@ -94,13 +153,21 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+`,
+        expected: 81,
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
