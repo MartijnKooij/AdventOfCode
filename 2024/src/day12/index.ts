f79d7c555f45e8e79019b0ff0ba624422fd7d716
Martijn Kooij <post@martijnkooij.nl>
2024-12-14 09:22:02 +0100

    Day 12 p2 WiP

--- 2024/src/day12/index.ts
+++ 2024/src/day12/index.ts
@@ -1,5 +1,6 @@
 import run from 'aocrunner';
 import { AocMap } from '../utils/map.js';
+import { dir } from 'console';
 
 type Point = { x: number; y: number };
 const parseInput = (rawInput: string) => new AocMap(rawInput);
@@ -75,6 +76,88 @@ const part1 = (rawInput: string) => {
   return sum;
 };
 
+const getPlots2 = (map: AocMap, startLocation: Point, visited: Set<string>, plot: Plot): void => {
+  const currentArea = map.get(startLocation.x, startLocation.y);
+  const currentLocationKey = `${startLocation.x},${startLocation.y},${currentArea}`;
+
+  visited.add(currentLocationKey);
+
+  for (const direction of directions) {
+    const nextLocation = {
+      x: startLocation.x + direction.x,
+      y: startLocation.y + direction.y,
+    };
+    if (
+      nextLocation.x < 0 ||
+      nextLocation.x >= map.columns ||
+      nextLocation.y < 0 ||
+      nextLocation.y >= map.rows
+    ) {
+      plot.perimeter++;
+      continue;
+    }
+
+    const nextArea = map.get(nextLocation.x, nextLocation.y);
+    if (nextArea !== currentArea) {
+      plot.perimeter++;
+    }
+
+    if (nextArea === currentArea) {
+      const nextLocationKey = `${nextLocation.x},${nextLocation.y},${nextArea}`;
+      if (!visited.has(nextLocationKey)) {
+        visited.add(nextLocationKey);
+        plot.size++;
+        getPlots2(map, nextLocation, visited, plot);
+      }
+    }
+  }
+};
+
+const calculatePerimeter = (visited: Set<string>, map: AocMap) => {
+  let corners = 0;
+  for (const key of visited) {
+    const [x, y] = key.split(',').map(Number);
+    const area = key.split(',')[2];
+
+    const top = { x: x - 1, y };
+    const right = { x, y: y + 1 };
+    const bottom = { x: x + 1, y };
+    const left = { x, y: y - 1 };
+    const topLeft = { x: x - 1, y: y - 1 };
+    const topRight = { x: x - 1, y: y + 1 };
+    const bottomLeft = { x: x + 1, y: y - 1 };
+    const bottomRight = { x: x + 1, y: y + 1 };
+
+    // Check all convex corners
+    if (
+      (top.x < 0 || top.x > map.columns - 1 || top.y < 0 || top.y > map.rows - 1 || map.get(top.x, top.y) !== area) &&
+      (right.x < 0 || right.x > map.columns - 1 || right.y < 0 || right.y > map.rows - 1 || map.get(right.x, right.y) !== area) &&
+      (topRight.x < 0 || topRight.x > map.columns - 1 || topRight.y < 0 || topRight.y > map.rows - 1 || map.get(topRight.x, topRight.y) === area)
+    ) {
+      corners++;
+    }
+
+    // Check all concave corners
+    if (
+      (top.x < 0 || top.x > map.columns - 1 || top.y < 0 || top.y > map.rows - 1 || map.get(top.x, top.y) === area) &&
+      (right.x < 0 || right.x > map.columns - 1 || right.y < 0 || right.y > map.rows - 1 || map.get(right.x, right.y) === area) &&
+      (topRight.x < 0 || topRight.x > map.columns - 1 || topRight.y < 0 || topRight.y > map.rows - 1 || map.get(topRight.x, topRight.y) !== area)
+    ) {
+      corners++;
+    }
+
+    // Check all diagonal corners
+    if (
+      (top.x < 0 || top.x > map.columns - 1 || top.y < 0 || top.y > map.rows - 1 || map.get(top.x, top.y) !== area) &&
+      (right.x < 0 || right.x > map.columns - 1 || right.y < 0 || right.y > map.rows - 1 || map.get(right.x, right.y) !== area) &&
+      (topRight.x < 0 || topRight.x > map.columns - 1 || topRight.y < 0 || topRight.y > map.rows - 1 || map.get(topRight.x, topRight.y) !== area)
+    ) {
+      corners++;
+    }
+  }
+  return corners;
+};
+
 const part2 = (rawInput: string) => {
   const map = parseInput(rawInput);
   const plots = [];
@@ -84,8 +167,9 @@ const part2 = (rawInput: string) => {
       if (map.get(x, y) !== '.') {
         const visited = new Set<string>();
         const plot = { size: 1, perimeter: 0, type: map.get(x, y) } as Plot;
-        getPlots(map, { x, y }, visited, plot);
-        plots.push(plot);
+        getPlots2(map, { x, y }, visited, plot);
+        plot.perimeter = calculatePerimeter(visited, map);
+        plots.push({ plot, visited });
 
         for (const key of visited) {
           const [x, y] = key.split(',');
@@ -99,7 +183,7 @@ const part2 = (rawInput: string) => {
 
   let sum = 0;
   for (const plot of plots.values()) {
-    sum += plot.size * plot.perimeter;
+    sum += plot.plot.size * plot.plot.perimeter;
   }
   return sum;
 };
