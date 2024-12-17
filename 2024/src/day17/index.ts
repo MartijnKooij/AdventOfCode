c540bb10215953585b30358d47fb869aff36fff1
Martijn Kooij <martijn.kooij@snappet.org>
2024-12-17 12:09:43 +0100

    Day 17 p1

--- 2024/src/day17/index.ts
+++ 2024/src/day17/index.ts
@@ -3,30 +3,59 @@ import run from 'aocrunner';
 type OpCode = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
 type RegisterType = 'A' | 'B' | 'C';
 type Register = Record<RegisterType, number>;
+
+const output: number[] = [];
 const getOperandValue = (operand: number, register: Register): number => {
   if (operand <= 3) return operand;
   if (operand === 4) return register.A;
   if (operand === 5) return register.B;
   if (operand === 6) return register.C;
+  if (operand === 7) return 7;
 
   throw new Error(`Invalid operand: ${operand}`);
 }
-const operations: Record<OpCode, (register: Register, operand: number) => void> = {
-  0: (register, operand) => register.A = register.A / Math.pow(2, getOperandValue(operand, register)),
-  1: (register, operand) => register.B = register.B ^ getOperandValue(operand, register),
-  2: (register, operand) => register.B = getOperandValue(operand, register) % 8,
-  3: (register, operand) => ,
-  4: (register, operand) => ,
-  5: (register, operand) => ,
-  6: (register, operand) => ,
-  7: (register, operand) => 
+const operations: Record<OpCode, (instruction: number, register: Register, operand: number) => number> = {
+  0: (instruction, register, operand) => { register.A = Math.trunc(register.A / Math.pow(2, getOperandValue(operand, register))); return instruction + 2; },
+  1: (instruction, register, operand) => { register.B = register.B ^ getOperandValue(operand, register); return instruction + 2; },
+  2: (instruction, register, operand) => { register.B = getOperandValue(operand, register) % 8; return instruction + 2; },
+  3: (instruction, register, operand) => register.A === 0 ? instruction + 2 : getOperandValue(operand, register),
+  4: (instruction, register, operand) => { register.B = register.B ^ register.C; return instruction + 2; },
+  5: (instruction, register, operand) => { output.push(getOperandValue(operand, register) % 8); return instruction + 2; },
+  6: (instruction, register, operand) => { register.B = Math.trunc(register.A / Math.pow(2, getOperandValue(operand, register))); return instruction + 2; },
+  7: (instruction, register, operand) => { register.C = Math.trunc(register.A / Math.pow(2, getOperandValue(operand, register))); return instruction + 2; },
 }
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) => {
+  const [registers, program] = rawInput.split('\n\n');
+  const register = registers.split('\n').reduce((acc, line) => {
+    const [reg, value] = line.split(': ');
+    acc[reg.replace('Register ', '') as RegisterType] = parseInt(value);
+    return acc;
+  }, {} as Register);
+
+  return {
+    register,
+    program: program.replace('Program: ', '').split(',').map(Number)
+  };
+};
 
 const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const app = parseInput(rawInput);
+  // const app = {
+  //   register: { A: 0, B: 29, C: 0 },
+  //   program: [1, 7]
+  // };
+  let instruction = 0;
+  output.length = 0;
+  while (true) {
+    const opcode = app.program[instruction];
+    const operand = app.program[instruction + 1];
+    // console.log('INSTRUCTION', instruction, opcode, operand);
+    instruction = operations[opcode as OpCode](instruction, app.register, operand);
+    // console.log('RESULT', app.register, instruction);
+    if (instruction >= app.program.length) break;
+  }
 
-  return;
+  return output.join(',');
 };
 
 const part2 = (rawInput: string) => {
@@ -60,5 +89,5 @@ Program: 0,1,5,4,3,0
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
