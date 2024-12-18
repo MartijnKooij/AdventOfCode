29e3becaf479f45d91b0f634383330a42ece1e1e
Martijn Kooij <post@martijnkooij.nl>
2024-12-18 20:30:34 +0100

    Day 16 p2 WiP

--- 2024/src/day16/index.ts
+++ 2024/src/day16/index.ts
@@ -52,7 +52,7 @@ const walk = (map: AocMap, start: Point): number => {
   }
   // There's an off by one error in the turns, I think when we start width a turn... Manually submitted the answer.
   console.log('Final score', finalScore, 'Final turns', finalTurns, 'Penalty', turnPenalty);
-  return finalScore + 1 + finalTurns * turnPenalty;
+  return finalScore + 1 + (finalTurns - 1) * turnPenalty;
 }
 
 const part1 = (rawInput: string) => {
@@ -72,15 +72,53 @@ const part1 = (rawInput: string) => {
   }
 
   const score = walk(input, start);
-  console.log(input.toString());
+  // console.log(input.toString());
 
   return score;
 };
 
+const walk2 = (map: AocMap, start: Point): { score: number, paths: Array<Array<Point>> } => {
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
+  const directions = ['<', 'v', '>', '^'];
+  const turnPenalty = 1000;
+  const queue = new TinyQueue([{ point: start, direction: '', turns: 0, score: 0, length: 0, path: [start] }], (a, b) => a.score - b.score);
+  const visited = new Set<string>();
+  const predecessor = new Map<string, { point: Point, direction: string }>();
+  const paths: Point[][] = [];
+
+  while (queue.length > 0) {
+    const { point: location, direction: currentDirection, score, turns, length, path } = queue.pop()!;
+    for (let i = 0; i < 4; i++) {
+      const nx = location.x + dx[i];
+      const ny = location.y + dy[i];
+      const next = map.tryGet(nx, ny);
+      const newDirection = directions[i];
+      const isTurn = currentDirection !== null && currentDirection !== newDirection;
+      const newStep = score + 1 + (isTurn ? turnPenalty : 0);
+
+      if (next === '.' && !visited.has(`${nx},${ny}`)) {
+        visited.add(`${nx},${ny}`);
+        predecessor.set(`${nx},${ny}`, { point: location, direction: newDirection });
+        queue.push({ point: { x: nx, y: ny } as Point, direction: newDirection, turns: turns + (isTurn ? 1 : 0), score: newStep, length: length + 1, path: [...path, { x: nx, y: ny }] });
+      }
+    }
+  }
+  return { score: queue.length, paths };
+};
+
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const start = input.find('S')!;
+  const result = walk2(input, start);
 
-  return;
+  let sum = 0;
+  console.log('Found', result.paths.length, 'paths');
+  for (const path of result.paths) {
+    sum += path.length;
+  }
+
+  return sum;
 };
 
 run({
@@ -121,10 +159,25 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############
+`,
+        expected: 45,
+      },
     ],
     solution: part2,
   },
